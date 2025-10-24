f2ce30eb9c13ba449e3dee7afd55f32ed2e1c55e
Marc Anton <marcanton.berschneider@gmail.com>
2025-10-24 12:04:46 +0200

    Create Start.java and Rechner.java classes

---
+++ auf7.1/Rechner.java
@@ -0,0 +1,9 @@
+public class Rechner
+{
+   public int hochDrei(int zahl)
+   {
+      zahl = zahl * zahl * zahl;
+      
+      return zahl;     
+   }
+}
---
+++ auf7.1/Start.java
@@ -0,0 +1,15 @@
+public class Start
+{
+   public static void main(String[] args)
+   {
+      int a = 5;
+      
+      Rechner rechner = new Rechner();
+
+      
+      int kubikzahl = rechner.hochDrei(a);
+      System.out.println(kubikzahl);
+       
+      
+   }
+}
