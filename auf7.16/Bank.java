2015464ad60407eebd4585ff4c4086a42f899d3b
Marc Anton <marcanton.berschneider@gmail.com>
2025-10-30 11:45:49 +0100

    Change user dialog from Bank.java

--- auf7.16/Bank.java
+++ auf7.16/Bank.java
@@ -4,16 +4,16 @@ public class Bank
 {
    public static void main(String[] args) throws Exception
    {
-      Konto konto1 = new Konto(00000001);
-      Konto konto2 = new Konto(00000002);
+      Konto konto1 = new Konto(1);
+      Konto konto2 = new Konto(2);
    
       // Eingabe des Anwenders wird eingelesen
       InputStreamReader eingabeLeser      = new InputStreamReader(System.in);
       BufferedReader    gepufferterLeser  = new BufferedReader(eingabeLeser);
-
-
       
+      int kontoNr =0;  
       boolean weitereAktionen = true;
+      boolean kontoExistiert  = false;
       
       while(weitereAktionen == true)
       {
@@ -24,25 +24,63 @@ public class Bank
          
          if(eingabe.equals("ja"))
          {
+            kontoExistiert = false;
+            while(kontoExistiert == false)
+            {
+               System.out.print("\n");
+               System.out.print("Welches Konto wollen Sie benutzen?: ");
+               eingabe = gepufferterLeser.readLine();
+               System.out.print("\n");
+               kontoNr = Integer.parseInt(eingabe);
+
+               if (kontoNr == 1 || kontoNr == 2)
+               {
+                  kontoExistiert = true;         
+               }
+            
+               if(kontoExistiert == false)
+               { 
+                  System.out.println("Dieses Konto existiert nicht");
+               }
+            }
+            
             System.out.print("Was wollen Sie tun? (e einzahlen / a auszahlen): "); 
             eingabe = gepufferterLeser.readLine();
             System.out.print("\n");
-
+         
             if (eingabe.equals("e"))
             {
-            System.out.print("Bitte geben Sie einen Betrag an: ");
-            eingabe = gepufferterLeser.readLine();
-            System.out.print("\n");
-            konto1.einzahlen(Integer.parseInt(eingabe));
-            konto1.kontoauszugAnzeigen();
+               System.out.print("Bitte geben Sie einen Betrag an: ");
+               eingabe = gepufferterLeser.readLine();
+               System.out.print("\n");
+               
+               if(kontoNr == 1)
+               {
+                  konto1.einzahlen(Integer.parseInt(eingabe));
+                  konto1.kontoauszugAnzeigen();
+               }
+               else if(kontoNr == 2)
+               {
+                  konto2.einzahlen(Integer.parseInt(eingabe));
+                  konto2.kontoauszugAnzeigen();
+               }
             }
             else if (eingabe.equals("a"))
             {
-            System.out.print("Bitte geben Sie einen Betrag an: ");
-            eingabe = gepufferterLeser.readLine();
-            System.out.print("\n");
-            konto1.abheben(Integer.parseInt(eingabe));
-            konto1.kontoauszugAnzeigen(); 
+               System.out.print("Bitte geben Sie einen Betrag an: ");
+               eingabe = gepufferterLeser.readLine();
+               System.out.print("\n");
+
+               if(kontoNr == 1)
+               {
+                  konto1.abheben(Integer.parseInt(eingabe));
+                  konto1.kontoauszugAnzeigen();
+               }
+               else if(kontoNr == 2)
+               {
+                  konto2.abheben(Integer.parseInt(eingabe));
+                  konto2.kontoauszugAnzeigen();
+               }
             }
             else
             {
